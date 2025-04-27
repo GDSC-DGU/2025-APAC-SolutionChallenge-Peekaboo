13697569b71ca6d2e07b3a56bceac6dde3f4a533
emjayMJkim <minjeong030420@gmail.com>
2025-04-28 00:57:48 +0900

    feat: application 설정
    application 설정 및 MainActivity 위치 이동

--- client/feature/src/main/java/com/peekaboo/feature/MainActivity.kt
+++ client/feature/src/main/java/com/peekaboo/feature/MainActivity.kt
@@ -1,31 +1,20 @@
-package com.peekaboo.baebae
+package com.peekaboo.feature
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.peekaboo.baebae.ui.theme.BaeBaeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BaeBaeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Greeting(name = "BaeBae")
         }
     }
 }
@@ -41,7 +30,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    BaeBaeTheme {
-        Greeting("Android")
-    }
+    Greeting(name = "BaeBae")
 }
