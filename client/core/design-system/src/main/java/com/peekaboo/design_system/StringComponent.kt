5f635d807f5e309bab236c1418e4367001caaf12
emjayMJkim <minjeong030420@gmail.com>
2025-05-01 14:06:46 +0900

    feat: core:ui 공통 컴포넌트 생성
    bottomRectangleBtn 구현
    courseNumber 구현
    selectItem 구현

--- client/core/design-system/src/main/java/com/peekaboo/design_system/StringComponent.kt
+++ client/core/design-system/src/main/java/com/peekaboo/design_system/StringComponent.kt
@@ -2,3 +2,5 @@ package com.peekaboo.design_system
 
 // Common
 const val BaeBae = "BaeBae"
+const val CourseNumberFormat = "%d/%d"
+
--- client/core/ui/build.gradle.kts
+++ client/core/ui/build.gradle.kts
@@ -9,4 +9,6 @@ android {
 
 dependencies {
     implementation(projects.core)
+    implementation(projects.core.designSystem)
+    implementation(projects.domain)
 }
