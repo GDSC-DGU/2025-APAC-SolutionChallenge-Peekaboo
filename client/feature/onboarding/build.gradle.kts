40abc1d44f47618c64f7d2f1d354f5a4308b239e
emjayMJkim <minjeong030420@gmail.com>
2025-05-01 13:04:44 +0900

    feat: feature:onboarding 모듈 추가
    onboarding 모듈 추가
    gradle 설정

---
+++ client/feature/onboarding/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("peekaboo.android.feature")
+    id("peekaboo.android.compose")
+}
+
+android {
+    namespace = "com.peekaboo.onboarding"
+}
+
+dependencies {
+    implementation(projects.domain)
+    implementation(projects.core)
+    implementation(projects.core.ui)
+    implementation(projects.core.designSystem)
+
+}
