5380d51c472dd86a67d84f656e19521c693f513f
emjayMJkim <minjeong030420@gmail.com>
2025-05-06 00:58:32 +0900

    feat: Set gradle version for google login
    Set versions libraries for google login and Apply to build gradle
    
    Related to: #24

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -17,4 +17,15 @@ dependencies {
     implementation(projects.domain)
     implementation(projects.core)
     implementation(projects.data)
+
+    implementation(libs.gson)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.okhttp.urlconnection)
+
+    implementation(libs.google.play.services.auth)
+    implementation(libs.google.credentail.service)
+    implementation(libs.google.credential)
+    implementation(libs.google.identity)
 }
--- client/feature/login/build.gradle.kts
+++ client/feature/login/build.gradle.kts
@@ -12,4 +12,9 @@ dependencies {
     implementation(projects.core)
     implementation(projects.core.ui)
     implementation(projects.core.designSystem)
+
+    implementation(libs.google.play.services.auth)
+    implementation(libs.google.credentail.service)
+    implementation(libs.google.credential)
+    implementation(libs.google.identity)
 }
