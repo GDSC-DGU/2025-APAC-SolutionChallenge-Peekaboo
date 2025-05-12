f9658e48fd78808b55023c003ef6a88c472bd862
choijian <an032944@daum.net>
2025-05-12 21:57:23 +0900

    feat: Add DISEASE ID MAP

--- ai-server/app/models/enums.py
+++ ai-server/app/models/enums.py
@@ -15,3 +15,20 @@ class ERole(str, Enum):
     @property
     def security_name(self) -> str:
         return f"ROLE_{self.value}"
+
+
+DISEASE_ID_MAP = {
+    "Miliaria": 1,
+    "Chickenpox": 2,
+    "Roseola": 3,
+    "Contact Dermatitis": 4,
+    "Milia": 5,
+    "Erythema Toxicum Neonatorum": 6,
+    "Aplasia Cutis Congenita": 7,
+    "Measles": 8,
+    "Atopic Dermatitis": 9,
+    "Seborrheic Dermatitis": 10,
+    "Impetigo": 11,
+    "Ringworm": 12,
+    "No Problem": 13,
+}
