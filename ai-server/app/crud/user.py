84779e9cf0f122158555c28357d710bf309627ee
choijian <an032944@daum.net>
2025-05-14 01:02:00 +0900

    fix: Change user id to UUID

--- ai-server/app/crud/user.py
+++ ai-server/app/crud/user.py
@@ -1,44 +1,49 @@
+import uuid
 from typing import Any
 
 from models.user import User
 from sqlalchemy.orm import Session
 
+from app.core.logging import logger
 
-def get_user(db: Session, user_id: int) -> User | None:
+
+def get_user(db: Session, id: str) -> User | None:
     """
-    Retrieve a User object from the database by user_id.
-
-    Args:
-        db (Session): SQLAlchemy database session.
-        user_id (int): ID of the user to retrieve.
-
-    Returns:
-        User | None: The User object if found, otherwise None.
+    Retrieve a User object from the database by id.
     """
-    return db.query(User).filter(User.id == user_id).first()
+    try:
+        user = db.query(User).filter(User.id == uuid.UUID(id).bytes).first()
+        if user:
+            logger.info(f"User {id} retrieved successfully.")
+        else:
+            logger.warning(f"User {id} not found.")
+        return user
+    except Exception:
+        logger.exception(f"Exception occurred while retrieving user {id}.")
+        return None
 
 
-def get_patient_info(db: Session, user_id: int) -> dict[str, Any]:
+def get_patient_info(db: Session, id: str) -> dict[str, Any]:
     """
     Retrieve patient information from the database.
-
-    Args:
-        db (Session): SQLAlchemy database session.
-        user_id (int): ID of the user to retrieve information for.
-
-    Returns:
-        dict: A dictionary containing patient information or an empty dict if not found.
     """
-    user = db.query(User).filter(User.id == user_id).first()
-    if user is None:
+    try:
+        user = db.query(User).filter(User.id == uuid.UUID(id).bytes).first()
+        if user is None:
+            logger.warning(f"Patient info for user {id} not found.")
+            return {}
+        logger.info(f"Patient info for user {id} retrieved successfully.")
+        return {
+            "location": user.location,
+            "birth": user.birth,
+            "gender": user.gender,
+            "blood_type": user.blood_type,
+            "skin_type": user.skin_type,
+            "allergies": [a.description for a in user.allergies],
+            "onboarding_diseases": [d.description for d in user.onboarding_diseases],
+        }
+    except Exception:
+        logger.exception(
+            f"Exception occurred while retrieving patient info for user {id}."
+        )
         return {}
-
-    return {
-        "location": user.location,
-        "birth": user.birth,
-        "gender": user.gender,
-        "blood_type": user.blood_type,
-        "skin_type": user.skin_type,
-        "allergies": user.allergies,
-        "onboarding_diseases": user.onboarding_diseases,
-    }
